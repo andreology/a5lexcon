9b5089902e2a11e529fe3895512b7b91f848e8f8
andreology <37252145+andreology@users.noreply.github.com>
2019-07-30 11:38:28 -0700

    Adding lexer and int

---
+++ src/lexer/Lexer.java
@@ -0,0 +1,34 @@
+package lexer;
+
+import java.util.ArrayList;
+
+/**         Lexer.java
+ *  Main runnable class that displays all the tokens proccessed by the Tokenizer.
+ *
+ *  @author Andre Barajas
+ */
+public class Lexer 
+{
+    public static void main(String args[]) {
+        Tokenizer tokenizer = null;
+        
+        //Check supplied command-line arguments(array of string objects)
+        if (args.length > 0)
+            tokenizer = new Tokenizer(args[0]);
+        else
+            tokenizer = new Tokenizer("program.txt");
+
+        ArrayList<Token> tokens = null;
+        if (tokenizer.isReady())
+            tokens = tokenizer.tokenize();
+
+        if (tokens != null && tokens.size() > 0) {
+            for (Token t : tokens) {
+                System.out.println(t);
+            }
+        } else
+            System.out.println("No tokens");
+
+        tokenizer.close();
+    }
+}
